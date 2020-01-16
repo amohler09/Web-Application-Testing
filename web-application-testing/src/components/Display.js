650966d6ba57fca17cb913283b8b64af100d596e
Amber Mohler <ambermohler@iMac.local>
2020-01-16 18:37:03 -0500

    restructured display component to match how i want to design it

--- web-application-testing/src/components/Display.js
+++ web-application-testing/src/components/Display.js
@@ -2,16 +2,36 @@ import React from 'react';
 
 const Display = props => {
     return (
-        <div className='display'>
-        <div className='balls-count'>
-            Balls: {props.ball}
-        </div>
-        <div className='strikes-count'>
-            Strikes: {props.strike}
-        </div>
-        <div className='hits-count'>
-            Hits: {props.hit}
-        </div>
+        <div className='scoreboard-container'>
+            <div className='scoreboard-top'>
+            <div className='home-score'>
+                <p>Home</p>
+                <p>0{props.hit}</p>
+            </div>
+            <div className='inning'>
+                <p>Inning</p>
+                <p>08</p>
+            </div>
+            <div className='guest-score'>
+                <p>Guest</p>
+                <p>22</p>
+            </div>
+            </div>
+
+            <div className='scoreboard-bottom'>
+                <div className='balls'>
+                    <p>Balls</p>
+                    <p>0{props.ball}</p>
+                </div>
+                <div className='strikes'>
+                    <p>Strikes</p>
+                    <p>0{props.strike}</p>
+                </div>
+                <div className='fouls'>
+                    <p>Fouls</p>
+                    <p>02</p>
+                </div>
+            </div>
         </div>
     )
 }
